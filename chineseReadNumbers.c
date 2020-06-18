e668bddef8f1c590683a0fa10db5af031f0f1ff8
zhangzhanli <32356175+zhangzhanli@users.noreply.github.com>
2020-06-18 16:06:02 +0800

    Create chineseReadNumbers.c

---
+++ chineseReadNumbers.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+
+char seps[]="\0wy";
+char bases[]="0sbq";
+char nums[]="0123456789";
+
+
+
+void f(int a, int b, int l){
+    if (a ==0)
+        return;
+    f(a / 10, b+1, a % 10);
+
+
+    if (b % 4 == 0){
+        if (a % 10){
+            printf("%c", nums[a%10]);
+        }
+        printf("%c",seps[b/4]);
+    }else{
+        if (a % 10){
+            printf("%c", nums[a%10]);
+            printf("%c", bases[b % 4]);
+        } else if ((l ? 1 : 0) ^ 0){
+            printf("l");
+        }
+    }
+}
+
+void f2(int c){
+    if (c < 0){
+        printf("fd");
+        c = -c;
+    }
+    f(c, 0, 0);
+
+}
+
+int main(int argc, char *argv[])
+{
+    f2(-98050905);
+    return 0;
+}
