4667086689e8ca7b92c41eb3d45cca070e99aec1
zhangzhanli <32356175+zhangzhanli@users.noreply.github.com>
2020-06-18 16:11:19 +0800

    Update chineseReadNumbers.c

--- chineseReadNumbers.c
+++ chineseReadNumbers.c
@@ -6,7 +6,7 @@ char nums[]="0123456789";
 
 
 
-void f(int a, int b, int l){
+void f(unsigned long long a, int b, int l){
     if (a ==0)
         return;
     f(a / 10, b+1, a % 10);
@@ -27,7 +27,7 @@ void f(int a, int b, int l){
     }
 }
 
-void f2(int c){
+void f2(long long  c){
     if (c < 0){
         printf("fd");
         c = -c;
@@ -38,6 +38,6 @@ void f2(int c){
 
 int main(int argc, char *argv[])
 {
-    f2(-98050905);
+    f2(-90108050905);
     return 0;
 }
